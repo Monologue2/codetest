eb39b0163e69c28c5b3bf2e295614530e4691aae
Yu-Seungeon <seyoumoon@gmail.com>
2023-10-20 21:15:21 +0900

    add comments

--- SSAFI/2072/main.py
+++ SSAFI/2072/main.py
@@ -1,10 +1,14 @@
+# Input 시 Line 단위
 T = int(input())
+
 
 for i in range(T):
     
     ans = 0
+    # List 내의 String에 Int() Mapping
     integers = list(map(int, input().split(" ")))
     for j in integers:
+        # if문 % /
         if (j%2 == 1):
             ans = ans + j
 
