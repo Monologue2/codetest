3de65e22500721fa0ddacb97cdb57a59f901764b
Yu-Seungeon <seyoumoon@gmail.com>
2023-10-20 21:32:25 +0900

    2070

---
+++ SSAFI/2070/main.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+for i in range(T):
+    integers = list(map(int, input().split(" ")))
+
+    a, b = integers[0], integers[1]
+
+    if a < b:
+        print(f"#{i+1} <")
+    elif a == b: 
+        print(f"#{i+1} =")
+    else :
+        print(f"#{i+1} >")
+    
